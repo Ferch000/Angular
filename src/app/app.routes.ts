c30b5309db7005d1c78f37cd4968ec11a5bf9397
Ferch000 <20223l001100@utcv.edu.mx>
2024-09-28 19:30:07 -0600

    fit: modificar Hola

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pagina/login.page').then((m) => m.loginPage),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login.page',
     pathMatch: 'full',
+  },
+  {
+    path: 'pagina',
+    loadComponent: () => import('./pagina/login.page').then( m => m.PaginaPage)
   },
 ];
